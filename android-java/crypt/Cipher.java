47f06754ddab65c07e7890f3b3c8be0e1871f375
berni <berni@extensa.pl>
2011-05-18 17:43:50 +0200

    refactoring

--- android-java/crypt/Cipher.java
+++ android-java/crypt/Cipher.java
@@ -1,9 +1,7 @@
 package crypt;
 
-import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
@@ -21,9 +19,9 @@ import javax.crypto.spec.SecretKeySpec;
  * @author Bernard Kobos
  */
 public class Cipher {
-	static String secret = "1234567890abcdef" ;
+	static String secret = "1234567890abcdef";
 
-	public static void setSkeySpec(SecretKeySpec skeySpec) {
+	public static void setSkeySpec(final SecretKeySpec skeySpec) {
 		Cipher.skeySpec = skeySpec;
 	}
 
@@ -32,7 +30,7 @@ public class Cipher {
 	private static int BLOCK_SIZE = 32;
 	private static char PADDING = '|' ;
 
-	static String nullPadString(String original) {
+	static String nullPadString(final String original) {
 		String output = original;
 		int remain = 0;
 		try {
@@ -71,7 +69,8 @@ public class Cipher {
 		}
 		if (ENCODE) {
 			// NO_WRAP to eliminate new line character at the end of string
-			encrypted = new String(Base64.encode(encryptedBytes,Base64.NO_WRAP));
+			encrypted = new String(
+					Base64.encode(encryptedBytes, Base64.NO_WRAP));
 		} else {
 			encrypted = new String(encryptedBytes);
 		}
@@ -86,7 +85,7 @@ public class Cipher {
 		byte[] encryptedBytes;
 		if (DECODE) {
 			encryptedBytes =
-				Base64.decode(ENCRYPTEDDATA.getBytes(),Base64.DEFAULT);
+				Base64.decode(ENCRYPTEDDATA.getBytes(), Base64.DEFAULT);
 		} else {
 			encryptedBytes = ENCRYPTEDDATA.getBytes();
 		}
@@ -102,39 +101,42 @@ public class Cipher {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		raw = new String(rawBytes,"UTF8");
+		raw = new String(rawBytes, "UTF8");
 		int delimiter = raw.indexOf(PADDING);
 		int length = Integer.valueOf(raw.substring(0, delimiter));
 		raw = raw.substring(delimiter + 1, length + delimiter + 1);
 		return raw;
 	}
 
-	public static String encrypt(String cleartext)
+	public static String encrypt(final String cleartext)
 	throws Exception {
-		if(skeySpec==null)
+		if (skeySpec == null) {
 			init();
+		}
 		byte[] result = encrypt(cleartext.getBytes("UTF8"));
 		return toHex(result);
 	}
 
-	public static String decrypt(String encrypted)
+	public static String decrypt(final String encrypted)
 	throws Exception {
-		if(skeySpec==null)
+		if (skeySpec == null){
 			init();
+		}
 		byte[] enc = toByte(encrypted);
 		byte[] result = decrypt(enc);
 		return new String(result,"UTF8");
 	}
 
-	public static void init(){
+	public static void init() {
 		try {
-			skeySpec = new SecretKeySpec(getRawKey("mylocalkey".getBytes()), "AES");
+			skeySpec = new SecretKeySpec(
+					getRawKey("mylocalkey".getBytes()), "AES");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	private static byte[] getRawKey(byte[] seed) throws Exception {
+	private static byte[] getRawKey(final byte[] seed) throws Exception {
 		KeyGenerator kgen = KeyGenerator.getInstance("AES");
 		SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
 		sr.setSeed(seed);
@@ -145,14 +147,14 @@ public class Cipher {
 	}
 
 
-	public static byte[] encrypt(byte[] clear) throws Exception {
+	public static byte[] encrypt(final byte[] clear) throws Exception {
 		javax.crypto.Cipher cipher = javax.crypto.Cipher.getInstance("AES");
 		cipher.init(javax.crypto.Cipher.ENCRYPT_MODE, skeySpec);
 		byte[] encrypted = cipher.doFinal(clear);
 		return encrypted;
 	}
 
-	public static byte[] decrypt( byte[] encrypted)
+	public static byte[] decrypt(final byte[] encrypted)
 	throws Exception {
 		javax.crypto.Cipher cipher = javax.crypto.Cipher.getInstance("AES");
 		cipher.init(javax.crypto.Cipher.DECRYPT_MODE, skeySpec);
@@ -160,26 +162,28 @@ public class Cipher {
 		return decrypted;
 	}
 
-	public static String toHex(String txt) {
+	public static String toHex(final String txt) {
 		return toHex(txt.getBytes());
 	}
 
-	public static String fromHex(String hex) {
+	public static String fromHex(final String hex) {
 		return new String(toByte(hex));
 	}
 
-	public static byte[] toByte(String hexString) {
+	public static byte[] toByte(final String hexString) {
 		int len = hexString.length() / 2;
 		byte[] result = new byte[len];
-		for (int i = 0; i < len; i++)
+		for (int i = 0; i < len; i++){
 			result[i] = Integer.valueOf(hexString.substring(2 * i, 2 * i + 2),
 					16).byteValue();
+		}
 		return result;
 	}
 
-	public static String toHex(byte[] buf) {
-		if (buf == null)
+	public static String toHex(final byte[] buf) {
+		if (buf == null) {
 			return "";
+		}
 		StringBuffer result = new StringBuffer(2 * buf.length);
 		for (int i = 0; i < buf.length; i++) {
 			appendHex(result, buf[i]);
@@ -189,7 +193,7 @@ public class Cipher {
 
 	private final static String HEX = "0123456789ABCDEF";
 
-	private static void appendHex(StringBuffer sb, byte b) {
+	private static void appendHex(final StringBuffer sb, final byte b) {
 		sb.append(HEX.charAt((b >> 4) & 0x0f)).append(HEX.charAt(b & 0x0f));
 	}
 
@@ -199,14 +203,15 @@ public class Cipher {
 			MessageDigest digest = java.security.MessageDigest
 			.getInstance("MD5");
 			digest.update(s.getBytes());
-			byte messageDigest[] = digest.digest();
+			byte[] messageDigest = digest.digest();
 
 			// Create Hex String
 			StringBuffer hexString = new StringBuffer();
 			for (int i = 0; i < messageDigest.length; i++) {
 				String h = Integer.toHexString(0xFF & messageDigest[i]);
-				while (h.length() < 2)
+				while (h.length() < 2) {
 					h = "0" + h;
+				}
 				hexString.append(h);
 			}
 			return hexString.toString();
@@ -218,27 +223,28 @@ public class Cipher {
 	}
 
 	/**
-	 * Fast and dirty command line - just for testing purposes
+	 * Fast and dirty command line - just for testing purposes.
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		String output = "";
 		int action = 0;
-		if(args[0].equalsIgnoreCase("-e")){
-			action = 1 ;
-		}else if(args[0].equalsIgnoreCase("-d")){
-			action = 2 ;
-		}else
+		if (args[0].equalsIgnoreCase("-e")) {
+			action = 1;
+		} else if (args[0].equalsIgnoreCase("-d")) {
+			action = 2;
+		} else {
 			System.exit(0);
+		}
 
 		StringBuffer fileData = new StringBuffer(1000);
 		InputStreamReader reader;
-		String s = null ;
 		try {
-			reader = new InputStreamReader(new FileInputStream(args[1]), "UTF-8");
+			reader = new InputStreamReader(
+					new FileInputStream(args[1]), "UTF-8");
 			char[] buf = new char[1024];
 			int numRead=0;
-			while((numRead=reader.read(buf)) != -1){
+			while ((numRead = reader.read(buf)) != -1) {
 				String readData = String.valueOf(buf, 0, numRead);
 				fileData.append(readData);
 				buf = new char[1024];
--- android-java/crypt/test/AesTest.java
+++ android-java/crypt/test/AesTest.java
@@ -1,57 +1,62 @@
 package crypt.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 import crypt.Cipher;
 
+/**
+ * Test Cipher coding capabilities.
+ * @author berni
+ *
+ */
 public class AesTest {
 
-	protected Cipher cipher;
-	String knownValues[][] = {
+	private Cipher cipher;
+	private String[][] knownValues = {
 				{"123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|} ",
 				 "MSGi2oLpyuzaLQSA+bxQtsmW6q7OFwYeOBKTBBwoM9QKCHfry9Y3Sw7i9yDxrm5RYe3fPUaW5j59Yj2clkLgUALllj1V2LuTV3L0FctLtDigkmHwlKjRvAKw/ISr5++R"
 				},
-				{"abcdd","e8SYh4uEoLMUJPawEh18urx7djjWG+ou+yNG5mr56MM="}
+				{"abcdd", "e8SYh4uEoLMUJPawEh18urx7djjWG+ou+yNG5mr56MM="}
 			};
 
 	@Test                                         
 	public final void testDecryptKnownValues() {       
 		for (String[] arr : knownValues) {
-			String encrypted_text = arr[1]; String decrypted_text = arr[0];
+			String encryptedText = arr[1]; String decryptedText = arr[0];
 			String result = null;
 			try {
-				result = Cipher.decryptString(encrypted_text,true);
+				result = Cipher.decryptString(encryptedText, true);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}          
-			assertEquals(decrypted_text, result);   			
+			assertEquals(decryptedText, result);   			
 		}
 	}
 	
 	@Test                                         
 	public final void testEncryptKnownValues() {       
 		for (String[] arr : knownValues) {
-			String clear_text = arr[0]; String encrypted_text = arr[1];
+			String clearText = arr[0]; String encryptedText = arr[1];
 			String result = null;
 			try {
-				result = Cipher.encryptString(clear_text,true);
+				result = Cipher.encryptString(clearText,true);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}   
-			assertEquals(encrypted_text, result);   			
+			assertEquals(encryptedText, result);   			
 		}
 	}
 	
 	
 	@Test
-	public final void testSanity(){
+	public final void testSanity() {
         // decrypt(encrypt(str))==str for all unicode chars
-        StringBuffer sb = new StringBuffer(12048);
-        for(int i=0; i<12048;i++){
-            sb.append((char)i);
+        StringBuffer sb = new StringBuffer(numOfUnicodeChars);
+        for (int i = 0; i < numOfUnicodeChars; i++) {
+            sb.append((char) i);
         }
-        String out = null ;
+        String out = null;
 		try {
 			Cipher.init();
 			out = Cipher.decrypt(Cipher.encrypt(sb.toString()));
@@ -62,41 +67,52 @@ public class AesTest {
 	}
 	
 	@Test
-	public final void testHexStringSanity(){
+	public final void testHexStringSanity() {
         // decrypt(encrypt(str))==str for all unicode chars
-        StringBuffer sb = new StringBuffer(12048);
-        for(int i=0; i<12048;i++){
-            sb.append((char)i);
+        StringBuffer sb = new StringBuffer(numOfUnicodeChars);
+        for (int i = 0; i < numOfUnicodeChars; i++) {
+            sb.append((char) i);
         }
-        String out = null ;
+        String out = null;
 		try {
 			Cipher.init();
-			out = Cipher.decryptString((Cipher.encryptString(sb.toString(),true)), true);
+			out = Cipher.decryptString((
+					Cipher.encryptString(sb.toString(), true)), true);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
         assertEquals(sb.toString(), out);
 	}
 	
+	int numOfUnicodeChars = 12048 ;
 	@Test
-	public final void testInnerSanity(){
+	public final void testInnerSanity() {
         // decrypt(encrypt(str))==str for all unicode chars
-        StringBuffer sb = new StringBuffer(12048);
-        for(int i=0; i<12048;i++){
-            sb.append((char)i);
+        StringBuffer sb = new StringBuffer(numOfUnicodeChars);
+        for (int i = 0; i < numOfUnicodeChars; i++) {
+            sb.append((char) i);
         }
-        byte[] enc = null ;
-        byte[] result = null ;
-        byte[] str = null ;
+        byte[] enc = null;
+        byte[] result = null;
+        byte[] str = null;
 		try {
 			Cipher.init();
-			str = sb.toString().getBytes("UTF8") ; 
+			str = sb.toString().getBytes("UTF8"); 
 			enc = Cipher.encrypt(str);
 			result = Cipher.decrypt(enc);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		for(int i=0;i<str.length;i++) 
+		for (int i = 0; i < str.length; i++) {
 			assertEquals(str[i], result[i]);
+		}
+	}
+
+	/**
+	 * Cipher getter.
+	 * @return cipher instance
+	 */
+	public final Cipher getCipher() {
+		return cipher;
 	}
 }
